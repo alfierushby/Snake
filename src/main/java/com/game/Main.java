70fcb4d63d2e430ad09ecabdf25d6b3f529aaa57
Alfie Rushby <psyar13@exmail.nottingham.ac.uk>
2022-11-14 21:56:22 +0000

    refactored testing framework to remove module on test, added maven test and deployment support.

--- src/main/java/com/game/Main.java
+++ src/main/java/com/game/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Hello world!" + GameUtil.class.getClassLoader().getResource("snake-head-right.png").toString());
+        System.out.println("Hello world!");
     }
 }
